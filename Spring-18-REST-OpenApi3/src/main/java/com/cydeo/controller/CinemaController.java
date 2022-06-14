d95792e1cfb7f73bbf23b2e21006b76fda2ec5ba
Mehmet <mathdenizi@gmail.com>
2022-06-14 21:53:36 +0200

    Spring REST       OPEN API Documentation

---
+++ Spring-18-REST-OpenApi3/src/main/java/com/cydeo/controller/CinemaController.java
@@ -0,0 +1,39 @@
+package com.cydeo.controller;
+
+import com.cydeo.entity.Cinema;
+import com.cydeo.repository.CinemaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "Cinema", description = "Cinema CRUD Operations")
+public class CinemaController {
+
+    private final CinemaRepository cinemaRepository;
+
+    public CinemaController(CinemaRepository cinemaRepository) {
+        this.cinemaRepository = cinemaRepository;
+    }
+
+    @GetMapping("/cinemas")
+    @Operation(summary = "Read all cinemas") //we add name the method
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved cinemas (OK)",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Something goes wrong", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @Content)
+    })
+    public List<Cinema> readAllCinemas(){
+        return cinemaRepository.findAll();
+    }
+
+}
