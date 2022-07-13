eac33e10709cce09e9b6b63059e12a8357a226fb
mathi <mathiasweb5180@gmail.com>
2022-07-13 15:43:37 +0200

    Unit testing Review

---
+++ Spring-21-UnitTesting-JUnit5/src/main/java/StringManipulation.java
@@ -0,0 +1,14 @@
+public class StringManipulation {
+
+    public static String concat(String a, String b){
+
+        return a+b;
+    }
+
+    public int length (String a, String b){
+
+        return (a+b).length();
+    }
+
+
+}
---
+++ Spring-21-UnitTesting-JUnit5/src/test/java/StringManipulationTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringManipulationTest {
+
+    @Test
+    void testCase1(){
+        fail("Not implemented yet");
+    }
+
+    @Test
+    void testCase2(){
+
+    }
+
+    @Test
+    void testCase3(){
+
+    }
+    @Test
+    void testCase4(){
+
+    }
+
+    @Test
+    void testCase5(){
+
+    }
+
+
+    @Test
+    void concat(){
+        String actual = StringManipulation.concat("al", "fa");
+    }
+
+}
